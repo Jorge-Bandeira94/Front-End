e44df608a384a2abacfeb02bc8c810116cc547d3
Jorge Luís <56854357+yurixquest@users.noreply.github.com>
2021-10-28 15:17:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Estrutura basica de JS
+// objetos-  ({carro: "gol", cor: "vermelho"})
+// funções- function()
+// funções - function liquidificador(frutas){aqui vem o código da função, frutas é a variável}
+// variavés em JS precisam ser designadas da seguinte forma: var nome = 'Jorge'
+// constantes são variaveis imutaveis: const name = "Jorge". caso se tente atribuir outro valor, irá ocasionar um erro
+//atribuindo um objeto const pessoa = { nome: "Jorge", idade: "27", falar: function() {alert(pessoa.name)}}
+// podemos fazer a função funcionar com pessoa.falar()
+
+//Agora vamos programa a função de Abrir menu do html ao click
+//DOM Document object model
+
+// Inicialmente vamos ancorar o seletor header nev na constante nav
+const nav = document.querySelector('#header nav')
+//Agora vamos criar a constante toggle pra procurar TUODO na tag nav que possui a classe toggle
+const toggle = document.querySelectorAll('nav .toggle')
+// sabemos que há dois toggles no html, um para abrir r outro para fechar o menu, dessa forma usmaos um laço de repetição (como em python). A sintaxe aqui é da seguinte forma:
+for (const element of toggle) {
+  // ao elemento adicionamos um evento(que será o click)
+  element.addEventListener('click', function () {
+    // aqui estamos indo no nav, em sua lista de classes, adicionando o método toggle que faz com que, se tiver show, ele retira, se não tiver, ele coloca (lá no html, entra automatico). Isso faz o meu aparecer ou desaparecer quando clicado
+    nav.classList.toggle('show')
+  })
+}
+
+// Quando clicar em um item do menu, esconder o menu
+// pesuisar todos os seletores em "a" dos li dos ul do nac
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    //vamos remover a classe show quando clicarmos em algum ponto do menu para ele sair da frente
+    nav.classList.remove('show')
+  })
+}
